95e8a94a77508065fe448b1f4c6dea70c8e516b0
syedabdullah56 <syedabdullah8750@gmail.com>
2025-11-01 14:19:14 +0500

    Push 19

--- app/page.js
+++ app/page.js
@@ -1,19 +1,8 @@
-"use client";
-
-import React from "react";
-
-import HeroComp from "../components/Hero";
-import AboutComp from "../components/About";
-import TestimonialsComp from "../components/Testimonials";
-import ContactComp from "../components/Contact";
-import ServicesComp from "../components/Services";
-
-// memo freeze to avoid re-render 
-const Hero = React.memo(HeroComp);
-const About = React.memo(AboutComp);
-const Testimonials = React.memo(TestimonialsComp);
-const Contact = React.memo(ContactComp);
-const Services = React.memo(ServicesComp);
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Testimonials from "../components/Testimonials";
+import Contact from "../components/Contact";
+import Services from "../components/Services";
 
 export default function Home() {
   return (
@@ -21,40 +10,9 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
-      <Testimonials />
-      <Contact />
+      <Testimonials /> 
+      <Contact />     
     </main>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import Hero from "../components/Hero";
-// import About from "../components/About";
-// import Testimonials from "../components/Testimonials";
-// import Contact from "../components/Contact";
-// import Services from "../components/Services";
-
-// export default function Home() {
-//   return (
-//     <main className="scroll-smooth">
-//       <Hero />
-//       <About />
-//       <Services />
-//       <Testimonials /> 
-//       <Contact />     
-//     </main>
-//   );
-// }
 
